531fe3a41800b3e8317baa8f9e209da515baf2fb
Gyustes <gaizkayus2006@gmail.com>
2025-10-30 14:57:36 +0100

    Cambio entero en JFrameListaPedidos. He rediseñado el JFrame. Aun falta terminarlo (ej. añadir tabla de historial de pedidos)

--- src/gui/JFrameListaPedidos.java
+++ src/gui/JFrameListaPedidos.java
@@ -2,17 +2,29 @@ package gui;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Component; 
 import java.awt.Dimension;
-import java.awt.Image;
-
-import javax.swing.ImageIcon;
+import java.awt.FlowLayout;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Image; 
+import java.awt.Insets;
+import javax.swing.BorderFactory;
+import javax.swing.Box;
+import javax.swing.BoxLayout;
 import javax.swing.JButton;
+import javax.swing.ImageIcon; 
+import javax.swing.JCheckBox; 
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
+import javax.swing.SwingUtilities;
 import javax.swing.WindowConstants;
 import javax.swing.table.DefaultTableModel;
+
 
 //LISTADO DE PEDIDOS
 
@@ -21,61 +33,168 @@ public class JFrameListaPedidos extends JFramePrincipal {
 	
 	
 	public JFrameListaPedidos() {
-		this.setTitle("Listado de pedidos");		
-		panel = new JPanel(new BorderLayout());
+		this.setTitle("Lista de Pedidos");
+		this.setSize(new Dimension(1000,750));
+		this.setLocationRelativeTo(null);
+		this.setDefaultCloseOperation(EXIT_ON_CLOSE); // Añadido para que cierre
 		
-		//CABECERA DE LA PAGINA
+		//Añadir la cabecera
+		this.add(crearPanelCabecera(), BorderLayout.NORTH);
 		
-				
-		JPanel cabeza = new JPanel(new BorderLayout());
-		panel.add(cabeza,BorderLayout.NORTH);
+		//Añadir el panel central
+		this.add(crearPanelCentral(), BorderLayout.CENTER);
 		
-		JPanel panel2 = new JPanel(new BorderLayout());
-		
-		ImageIcon logo = new ImageIcon("resources/images/Carrito.png");
-		ImageIcon logoAjustado = new ImageIcon(logo.getImage().getScaledInstance(40, 40, Image.SCALE_SMOOTH));
-		JLabel imagen = new JLabel(logoAjustado);
-		cabeza.add(imagen, BorderLayout.EAST);
-		
-		
-		ImageIcon logo2 = new ImageIcon("resources/images/Casa.png");
-		ImageIcon logoAjustado2 = new ImageIcon(logo2.getImage().getScaledInstance(40, 40, Image.SCALE_SMOOTH));
-		JLabel imagen2 = new JLabel(logoAjustado2);
-		cabeza.add(imagen2, BorderLayout.WEST);
-		
-		
-		
-		//TABLA
-		String[] cabecera = {"numero pedido", "Fecha de la orden", "Fecha de llegada", "Numero poductos","Productos totales"};
-		
-		
-		DefaultTableModel modelo = new DefaultTableModel(cabecera, 1) {
-		    @Override
-		    public boolean isCellEditable(int row, int column) {
-		        return false;
-		    }
-		};
-		
-		JTable pedidos = new JTable(modelo);
-		
-		pedidos.getTableHeader().setReorderingAllowed(false);
-		JScrollPane scroll = new JScrollPane(pedidos);
-		 	
-		panel2.add(scroll, BorderLayout.CENTER);
-				
-		modelo.addRow(new Object[]{
-			    "P-002", "2025-10-28", "2025-11-05", "4", "125.50 €"
-			});
-		
-		
-		//panel.add(panel2, BorderLayout.CENTER);
-		this.add(panel);
-			
+		this.setVisible(true);
 	}
+	
+	private JPanel crearPanelCabecera() {
+		JPanel panelCabecera = new JPanel(new BorderLayout());
+		panelCabecera.setBorder(BorderFactory.createEmptyBorder(5,5,5,5));
+		
+		JPanel panelFiltro = new JPanel(new FlowLayout(FlowLayout.RIGHT));
+		JTextField txtfiltro = new JTextField(10);
+		panelFiltro.add(new JLabel("Filtrado por id: "));
+		panelFiltro.add(txtfiltro);
+		
+		JLabel precioTotal = new JLabel("0.00", SwingConstants.RIGHT);
+		precioTotal.setBorder(BorderFactory.createLineBorder(Color.GRAY));
+		precioTotal.setOpaque(true); 
+		precioTotal.setBackground(Color.WHITE);
+		precioTotal.setPreferredSize(new Dimension(100, 20));
+		
+				
+		panelFiltro.add(new JLabel("Valor total: "));
+		panelFiltro.add(precioTotal);
+		panelCabecera.add(panelFiltro, BorderLayout.EAST);
+		
+		return panelCabecera;
+		
+	}
+	
+	private JPanel crearPanelCentral() {
+		JPanel panelCentral = new JPanel(new GridBagLayout());
+		GridBagConstraints gbc = new GridBagConstraints();
+		gbc.insets = new Insets(5,5,5,5); //Para la separacion entre los componentes que se añaden
+		
+		JPanel Proveedores = new JPanel(new FlowLayout(FlowLayout.LEFT));
+		JTextField txtfiltro = new JTextField(10);
+		Proveedores.add(new JLabel("Filtrado por proveedor: "));
+		Proveedores.add(txtfiltro);
+		gbc.gridx = 0;
+		gbc.gridy = 0;
+		gbc.gridwidth = 1; 
+		gbc.weightx = 0.0; 
+		gbc.weighty = 0.0; 
+		gbc.fill = GridBagConstraints.HORIZONTAL; 
+		gbc.anchor = GridBagConstraints.WEST;
+        panelCentral.add(Proveedores, gbc);
+        
+        gbc.gridx = 1;
+        gbc.weightx = 1.0; 
+        panelCentral.add(new JLabel(""), gbc);
+        
+        String[] cabecera = {"numero pedido", "Fecha de la orden", "Fecha de llegada", "Numero poductos","Productos totales"};
+        DefaultTableModel model = new DefaultTableModel(cabecera, 10) {
+        	@Override
+        	public boolean isCellEditable(int row, int column) {
+        		return false;
+        	}
+        };
+        
+        JTable tablaPedidos = new JTable(model);
+        tablaPedidos.getTableHeader().setReorderingAllowed(false);
+        
+		JScrollPane scroll = new JScrollPane(tablaPedidos);
+		
+		gbc.gridx = 0;
+		gbc.gridy = 1;
+		gbc.gridwidth = 2;
+		gbc.weightx = 1.0;
+		gbc.weighty = 1.0; 
+		gbc.fill = GridBagConstraints.BOTH;
+		panelCentral.add(scroll, gbc);
+		
+		JPanel OpcionesInferior = crearOpcionesInferior(); 
+		
+		gbc.gridx=0;
+		gbc.gridy= 2;
+		gbc.gridwidth = 2;
+		gbc.weightx = 1.0;
+		gbc.weighty = 0.0; 
+		
+		gbc.fill= GridBagConstraints.HORIZONTAL; 
+		
+		panelCentral.add(OpcionesInferior,gbc);
+		
+		return panelCentral;
+		
+	}
+	
+	private JPanel crearOpcionesInferior() {
+	    JPanel panelOpciones = new JPanel(new GridBagLayout());
+	    GridBagConstraints gbc = new GridBagConstraints();
+	    gbc.insets = new Insets(5,5,5,5);
+	    
+	    gbc.weighty = 1.0; 
+	    
+	    JPanel panelAnadir = crearAñadir();
+	    gbc.gridx = 0;
+	    gbc.weightx = 0.3;
+	    gbc.fill = GridBagConstraints.BOTH; 
+	    panelOpciones.add(panelAnadir, gbc);
+	    
+	    
+	    JPanel panelHistorial = crearHistorialPedido();
+	    gbc.gridx = 1;
+	    gbc.weightx = 0.7; 
+	    gbc.fill = GridBagConstraints.BOTH; 
+	    panelOpciones.add(panelHistorial, gbc);
+	    
+	    return panelOpciones;
+	}
+	
+	private JPanel crearAñadir() {
+	    JPanel panelañadir = new JPanel();
+	    panelañadir.setLayout(new BoxLayout(panelañadir, BoxLayout.Y_AXIS));
+	    panelañadir.setBorder(BorderFactory.createTitledBorder("Añadir y elimina pedidos"));
+	    
+	    panelañadir.setPreferredSize(new Dimension(200, 150));
+	    
+	    JButton anadir = new JButton("Añadir Pedido");
+	    JButton eliminar = new JButton("Eliminar Pedido");
+	    
+	    // Centrar botones horizontalmente
+	    anadir.setAlignmentX(Component.CENTER_ALIGNMENT);
+	    eliminar.setAlignmentX(Component.CENTER_ALIGNMENT);
+	    
+	    panelañadir.add(Box.createVerticalGlue()); 
+	    panelañadir.add(anadir);
+	    panelañadir.add(Box.createVerticalStrut(10));
+	    panelañadir.add(eliminar);
+	    panelañadir.add(Box.createVerticalGlue()); 
 
+	    return panelañadir;
+	    
+	}
+	
+	private JPanel crearHistorialPedido() {
+	    JPanel panelhistorial = new JPanel();
+	    panelhistorial.setLayout(new BoxLayout(panelhistorial, BoxLayout.Y_AXIS));
+	    panelhistorial.setBorder(BorderFactory.createTitledBorder("Historial de pedidos"));
+	    
+	    panelhistorial.setPreferredSize(new Dimension(400, 150));
+	    
+	    JLabel tablon = new JLabel("Aqui va una tabla con los datos de los pedidos ya entregados");
+	    tablon.setAlignmentX(Component.CENTER_ALIGNMENT);
+	    
+	    panelhistorial.add(Box.createVerticalGlue());
+	    panelhistorial.add(tablon);
+	    panelhistorial.add(Box.createVerticalGlue());
+	    
+	    return panelhistorial;
+	}
+	
 	public static void main(String[] args) {
-		JFrameListaPedidos lista = new JFrameListaPedidos();
-		lista.setVisible(true);
-
+		SwingUtilities.invokeLater(() -> new JFrameListaPedidos());
 	}
 }
