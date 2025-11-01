faca134bda849d4dd835755fc4e0989c961f7781
Gyustes <gaizkayus2006@gmail.com>
2025-11-02 00:14:03 +0100

    Se ha cambiado la forma de dataHistorial

--- src/domain/DataHistorial.java
+++ src/domain/DataHistorial.java
@@ -3,43 +3,130 @@ package domain;
 import java.util.Vector;
 
 public class DataHistorial {
-	public static Vector<Vector<Object>> cargarHistorial(){
-		Vector<Vector<Object>> datos = new Vector<>();
 
-        datos.add(crearFila("001", "2025-10-05", "15"));
-        datos.add(crearFila("002", "2025-10-06", "20"));
-        datos.add(crearFila("003", "2025-10-07", "10"));
-        datos.add(crearFila("004", "2025-10-08", "25"));
-        datos.add(crearFila("005", "2025-10-09", "30"));
-        datos.add(crearFila("006", "2025-10-10", "15"));
-        datos.add(crearFila("007", "2025-10-11", "20"));
-        datos.add(crearFila("008", "2025-10-12", "25"));
-        datos.add(crearFila("009", "2025-10-13", "10"));
-        datos.add(crearFila("010", "2025-10-14", "35"));
-        datos.add(crearFila("011", "2025-10-15", "15"));
-        datos.add(crearFila("012", "2025-10-16", "20"));
-        datos.add(crearFila("013", "2025-10-17", "25"));
-        datos.add(crearFila("014", "2025-10-18", "30"));
-        datos.add(crearFila("015", "2025-10-19", "15"));
-        datos.add(crearFila("016", "2025-10-20", "20"));
-        datos.add(crearFila("017", "2025-10-21", "10"));
-        datos.add(crearFila("018", "2025-10-22", "25"));
-        datos.add(crearFila("019", "2025-10-23", "20"));
-        datos.add(crearFila("020", "2025-10-24", "30"));
+    public static Vector<Vector<Object>> cargarHistorial() {
+        Vector<Vector<Object>> data = new Vector<>();
 
-		
-		return datos;
-		
-	};
-	
-	public static Vector<Object> crearFila(String id, String fecha, String productos){
-		Vector<Object> fila = new Vector<>();
-		fila.add(id);
-		fila.add(fecha);
-		fila.add(productos);
-		
-		
-		return fila;
-		
-	}
+        Vector<Object> histA = new Vector<>();
+        histA.add("001");
+        histA.add("2025-10-05");
+        histA.add("15");
+        data.add(histA);
+
+        Vector<Object> histB = new Vector<>();
+        histB.add("002");
+        histB.add("2025-10-06");
+        histB.add("20");
+        data.add(histB);
+
+        Vector<Object> histC = new Vector<>();
+        histC.add("003");
+        histC.add("2025-10-07");
+        histC.add("10");
+        data.add(histC);
+
+        Vector<Object> histD = new Vector<>();
+        histD.add("004");
+        histD.add("2025-10-08");
+        histD.add("25");
+        data.add(histD);
+
+        Vector<Object> histE = new Vector<>();
+        histE.add("005");
+        histE.add("2025-10-09");
+        histE.add("30");
+        data.add(histE);
+
+        Vector<Object> histF = new Vector<>();
+        histF.add("006");
+        histF.add("2025-10-10");
+        histF.add("15");
+        data.add(histF);
+
+        Vector<Object> histG = new Vector<>();
+        histG.add("007");
+        histG.add("2025-10-11");
+        histG.add("20");
+        data.add(histG);
+
+        Vector<Object> histH = new Vector<>();
+        histH.add("008");
+        histH.add("2025-10-12");
+        histH.add("25");
+        data.add(histH);
+
+        Vector<Object> histI = new Vector<>();
+        histI.add("009");
+        histI.add("2025-10-13");
+        histI.add("10");
+        data.add(histI);
+
+        Vector<Object> histJ = new Vector<>();
+        histJ.add("010");
+        histJ.add("2025-10-14");
+        histJ.add("35");
+        data.add(histJ);
+
+        Vector<Object> histK = new Vector<>();
+        histK.add("011");
+        histK.add("2025-10-15");
+        histK.add("15");
+        data.add(histK);
+
+        Vector<Object> histL = new Vector<>();
+        histL.add("012");
+        histL.add("2025-10-16");
+        histL.add("20");
+        data.add(histL);
+
+        Vector<Object> histM = new Vector<>();
+        histM.add("013");
+        histM.add("2025-10-17");
+        histM.add("25");
+        data.add(histM);
+
+        Vector<Object> histN = new Vector<>();
+        histN.add("014");
+        histN.add("2025-10-18");
+        histN.add("30");
+        data.add(histN);
+
+        Vector<Object> histO = new Vector<>();
+        histO.add("015");
+        histO.add("2025-10-19");
+        histO.add("15");
+        data.add(histO);
+
+        Vector<Object> histP = new Vector<>();
+        histP.add("016");
+        histP.add("2025-10-20");
+        histP.add("20");
+        data.add(histP);
+
+        Vector<Object> histQ = new Vector<>();
+        histQ.add("017");
+        histQ.add("2025-10-21");
+        histQ.add("10");
+        data.add(histQ);
+
+        Vector<Object> histR = new Vector<>();
+        histR.add("018");
+        histR.add("2025-10-22");
+        histR.add("25");
+        data.add(histR);
+
+        Vector<Object> histS = new Vector<>();
+        histS.add("019");
+        histS.add("2025-10-23");
+        histS.add("20");
+        data.add(histS);
+
+        Vector<Object> histT = new Vector<>();
+        histT.add("020");
+        histT.add("2025-10-24");
+        histT.add("30");
+        data.add(histT);
+
+        return data;
+    }
 }
