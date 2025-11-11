0e57a09d37f0a7b67e421ab6cc5ccdee84258195
LibertoV <liberto.vazquez@opendeusto.es>
2025-11-11 11:05:56 +0100

    Correcion logo pantalla Lobby

--- src/gui/JFrameLobby.java
+++ src/gui/JFrameLobby.java
@@ -48,18 +48,29 @@ public class JFrameLobby extends JFramePrincipal {
         this.setFocusable(true); //IAG
         this.addKeyListener(listenerVolver(JFrameLogin.class));
         ImageIcon logo = new ImageIcon("resources/images/logoEmpresa1.png");
-        ImageIcon logoAjustado = new ImageIcon(logo.getImage().getScaledInstance(90, 60, Image.SCALE_SMOOTH));
+       
+        ImageIcon logoAjustado = new ImageIcon(logo.getImage().getScaledInstance(75, 50, Image.SCALE_SMOOTH));
         JLabel imagen = new JLabel(logoAjustado);
-        
         JButton loginButton = new JButton("Cerrar sesión");
-        JPanel SelectionPanel = new JPanel(new FlowLayout());
+        JPanel logoPanel = new JPanel(new FlowLayout());
+        logoPanel.add(imagen);
+        logoPanel.add(new JLabel("Farmacias"));
+        logoPanel.setOpaque(false);
         
+        
+        JPanel login = new JPanel(new FlowLayout());
+        login.add(loginButton);
+        login.setOpaque(false);
+        
+        
+        JPanel SelectionPanel = new JPanel(new BorderLayout());
         SelectionPanel.setOpaque(false);
         SelectionPanel.setPreferredSize(new Dimension(SelectionPanel.getPreferredSize().width, 50));
    
-        SelectionPanel.add(imagen);
-        SelectionPanel.add(new JLabel("Farmacias"));
-        SelectionPanel.add(loginButton,FlowLayout.RIGHT);
+        
+        
+        SelectionPanel.add(logoPanel,BorderLayout.WEST);
+        SelectionPanel.add(login,BorderLayout.EAST);
         
         
         Vector<String> columnNames = new Vector<>();
